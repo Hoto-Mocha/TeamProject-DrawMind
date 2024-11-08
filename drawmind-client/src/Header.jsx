6b6a44a238c6d0498c8db5d254686900c997101f
Hoto-Mocha <mtj2010@naver.com>
2024-11-08 11:31:08 +0900

    헤더 디자인 진행 중 업로드

--- drawmind-client/src/Header.jsx
+++ drawmind-client/src/Header.jsx
@@ -1,63 +1,36 @@
+import React from 'react';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom'
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import "./css/Header.css"
 
-const options = [
-  {
-    name: 'Enable backdrop (default)',
-    scroll: false,
-    backdrop: true,
-  },
-  {
-    name: 'Disable backdrop',
-    scroll: false,
-    backdrop: false,
-  },
-  {
-    name: 'Enable body scrolling',
-    scroll: true,
-    backdrop: false,
-  },
-  {
-    name: 'Enable both scrolling & backdrop',
-    scroll: true,
-    backdrop: true,
-  },
-];
-
-function OffCanvasExample({ name, ...props }) {
+export default function Header() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const toggleShow = () => setShow((s) => !s);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={toggleShow} className="me-2">
-        {name}
-      </Button>
-      <Offcanvas show={show} onHide={handleClose} {...props}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
 
-function Header() {
-  return (
-    <>
-      {options.map((props, idx) => (
-        <OffCanvasExample key={idx} {...props} />
-      ))}
+      <div className="headerBox">
+        <img className="menuIcon" src="./src/assets/icons/menuIcon.png" onClick={handleShow} />
+        <Offcanvas show={show} onHide={handleClose} style={{"width":"70%"}}>
+          <Offcanvas.Header closeButton style={{"borderBottom":"3px solid green"}}>
+            <Offcanvas.Title>메뉴</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body style={{"padding":"0"}}>
+            <div className='offBoxNav'>
+              <a href="/">로그인</a>
+            </div>
+            <div className='offBoxNav'>
+              <a href="/">회원 가입</a>
+            </div>
+          </Offcanvas.Body>
+        </Offcanvas>
+        <h2>MindDraw</h2>
+        <button className="btn btn-sm generalBtn">글쓰기</button>
+      </div>
     </>
-  );
+  )
 }
-
-render(<Header />);
-export default Header;
