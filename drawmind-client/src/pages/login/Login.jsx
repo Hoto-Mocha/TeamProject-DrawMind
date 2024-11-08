ed14b6341671babb1b2a3b4ed06cb35ffd845e6e
dobby <tmakdlf2482@naver.com>
2024-11-08 15:55:27 +0900

    로그인 화면 중간 업로드2

--- drawmind-client/src/pages/login/Login.jsx
+++ drawmind-client/src/pages/login/Login.jsx
@@ -7,19 +7,37 @@ import './Login.css';
 function Login() {
   return (
     <>
+      {/* 열쇠 아이콘 부분 */}
       <div className='loginHeader'>
         <FaKey style={{ width: '500px', height: '100px', color: 'skyblue' }} />
       </div>
+
+      {/* 아이디, 비밀번호 입력 부분 */}
       <div className='loginBody'>
         <InputGroup size="sm" className="mb-3" style={{ border: '1px solid #BDBDBD', width: '70%' }}>
           <Form.Control
+            className='input'
             aria-label="Small"
             aria-describedby="inputGroup-sizing-sm"
+            placeholder='아이디'
+          />
+        </InputGroup>
+
+        <InputGroup size="sm" className="mb-3" style={{ border: '1px solid #BDBDBD', width: '70%' }}>
+          <Form.Control
+            className='input'
+            aria-label="Small"
+            aria-describedby="inputGroup-sizing-sm"
+            placeholder='비밀번호'
+            type='password'
           />
         </InputGroup>
       </div>
+
+      {/* 로그인, 회원가입 버튼 부분 */}
       <div className='loginFooter'>
-        
+        <button className="btn btn-md loginBtn">로그인</button>
+        <button className="btn btn-md registerBtn">회원가입</button>
       </div>
     </>
   );
