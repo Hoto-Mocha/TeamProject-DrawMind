b43847ad1585c0351d6ef18532317790f7fc86f0
Hoto-Mocha <mtj2010@naver.com>
2024-11-08 14:50:55 +0900

    메인 페이지 디자인 중간 업로드

--- drawmind-client/src/App.jsx
+++ drawmind-client/src/App.jsx
@@ -1,11 +1,31 @@
+import "./App.css"
+
 export default function App() {
+
+  const data = [
+    { "postSeq": 1, "postTitle": "게시글 제목 1", "writer": "관리자", "regDate": "2024-11-08 14:10" },
+    { "postSeq": 2, "postTitle": "게시글 제목 2", "writer": "관리자", "regDate": "2024-11-08 14:12" },
+    { "postSeq": 3, "postTitle": "게시글 제목 3", "writer": "관리자", "regDate": "2024-11-08 14:14" }
+  ]
+
+  function postItem(item) {
+    return (
+      <>
+        <div className="infoSection">
+          <p>{item.postTitle}</p>
+          <p>{item.writer}</p>
+        </div>
+        <p>{item.regDate}</p>
+      </>
+    )
+  }
 
   return (
     <>
       <div>
-        <h1>hello world</h1>
-        <p>처음 메인 페이지입니다.</p>
-        <span>11.07 김동윤 작성</span>
+        {data.map((item, index) => {
+          return (<div className="postListItem" key={index}>{postItem(item)}</div>)
+        })}
       </div>
     </>
   )
