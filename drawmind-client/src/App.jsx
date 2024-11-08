9b932dd25c2c7e5612bb51f6c94d232bed7137d6
Hoto-Mocha <mtj2010@naver.com>
2024-11-08 15:15:25 +0900

    게시글 리스트 페이지 디자인 수정

--- drawmind-client/src/App.jsx
+++ drawmind-client/src/App.jsx
@@ -3,9 +3,27 @@ import "./App.css"
 export default function App() {
 
   const data = [
-    { "postSeq": 1, "postTitle": "게시글 제목 1", "writer": "관리자", "regDate": "2024-11-08 14:10" },
-    { "postSeq": 2, "postTitle": "게시글 제목 2", "writer": "관리자", "regDate": "2024-11-08 14:12" },
-    { "postSeq": 3, "postTitle": "게시글 제목 3", "writer": "관리자", "regDate": "2024-11-08 14:14" }
+    { "postSeq": 1, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:10" },
+    { "postSeq": 2, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:12" },
+    { "postSeq": 3, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 4, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 5, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 6, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 7, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 8, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 9, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 10, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 11, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 12, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 13, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 14, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 15, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 16, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 17, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 18, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 19, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 20, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
+    { "postSeq": 21, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
   ]
 
   function postItem(item) {
@@ -22,7 +40,7 @@ export default function App() {
 
   return (
     <>
-      <div>
+      <div className="postList">
         {data.map((item, index) => {
           return (<div className="postListItem" key={index}>{postItem(item)}</div>)
         })}
