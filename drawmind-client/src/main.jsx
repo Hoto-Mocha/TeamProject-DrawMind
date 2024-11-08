116d7bc8fb33455b112d4a304b53b0a2159d8940
dobby <tmakdlf2482@naver.com>
2024-11-08 17:35:02 +0900

    회원가입 화면

--- drawmind-client/src/main.jsx
+++ drawmind-client/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
 import Layout from './components/common/Layout.jsx'
 import Login from './pages/login/Login.jsx'
-import Register from './pages/Register.jsx'
+import Register from './pages/register/Register.jsx'
 import Write from './pages/Write.jsx'
 import Edit from './pages/Edit.jsx'
 
