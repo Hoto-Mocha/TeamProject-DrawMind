db46d2275d9de74eaa9c89eac285639fe88dcb0f
choej2303 <chlwnsgur22@gmail.com>
2024-11-08 16:23:31 +0900

    캔버스 작성 중. 색깔 선택, undo, redo, clear(화면 초기화) 기능 구현됨.

--- drawmind-client/src/MyCanvas.jsx
+++ drawmind-client/src/MyCanvas.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import Palette from "./Palette.jsx";
 import './css/MyCanvas.css';
 
-function MyCanvas({ postRef }) {
+function MyCanvas({postRef}) {
     const canvasRef = useRef(null);
     const [isDrawing, setDrawing] = useState(false);
     const contextRef = useRef(null);
@@ -12,6 +12,7 @@ function MyCanvas({ postRef }) {
     const [undoSteps, setUndoSteps] = useState([]);
     const [redoSteps, setRedoSteps] = useState([]);
 
+    // 화면 크기 조절에 대응
     const resizeCanvas = () => {
         const postElement = postRef.current;
         const canvasElement = canvasRef.current;
@@ -37,16 +38,32 @@ function MyCanvas({ postRef }) {
         }
     };
 
+    // 그리기 기능
     const draw = (e) => {
         if (!isDrawing) return;
-        const { offsetX, offsetY } = e.nativeEvent;
-        step.push({ offsetX, offsetY, color: contextRef.current.strokeStyle });
+        const {offsetX, offsetY} = e.nativeEvent;
+        step.push({offsetX, offsetY, color: contextRef.current.strokeStyle});
         contextRef.current.lineTo(offsetX, offsetY);
         contextRef.current.stroke();
         contextRef.current.beginPath();
         contextRef.current.moveTo(offsetX, offsetY);
     };
 
+    // 그리기 기능 on Mobile
+    function drawMobile(e) {
+        if (!isDrawing) return;
+        const touch = e.touches[0];
+        const rect = canvasRef.current.getBoundingClientRect();
+        const offsetX = touch.clientX - rect.left;
+        const offsetY = touch.clientY - rect.top;
+        step.push({offsetX, offsetY, color: contextRef.current.strokeStyle});
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+    }
+
+    // 창 크기 조절 시 resizeCanvas를 호출함
     useEffect(() => {
         resizeCanvas();
         window.addEventListener("resize", resizeCanvas);
@@ -55,13 +72,14 @@ function MyCanvas({ postRef }) {
         };
     }, []);
 
+    // 저장된 기록을 기반으로 그림 다시 그리기
     const drawPath = () => {
         const context = canvasRef.current.getContext("2d");
         context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
         context.beginPath();
 
         for (const path of undoSteps) {
-            for (const { offsetX, offsetY, color, clear } of path) {
+            for (const {offsetX, offsetY, color, clear} of path) {
                 if (clear === true) {
                     context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
                 } else {
@@ -75,10 +93,12 @@ function MyCanvas({ postRef }) {
         }
     };
 
+    //
     useEffect(() => {
         drawPath();
     }, [undoSteps]);
 
+    // undo 기능
     const undo = () => {
         setUndoSteps((prevSteps) => {
             if (prevSteps.length === 0) return prevSteps;
@@ -88,6 +108,7 @@ function MyCanvas({ postRef }) {
         });
     };
 
+    // redo 기능
     const redo = () => {
         setRedoSteps((prevRedoSteps) => {
             if (prevRedoSteps.length === 0) return prevRedoSteps;
@@ -97,15 +118,18 @@ function MyCanvas({ postRef }) {
         });
     };
 
+    // 화면 초기화 기능
     const clear = () => {
         contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        setUndoSteps((prevSteps) => [...prevSteps, [{ clear: true }]]);
+        setUndoSteps((prevSteps) => [...prevSteps, [{clear: true}]]);
     };
 
+
     return (
-        <div className="myCanvas" style={{ position: "absolute" }}>
+        <div className="myCanvas" style={{position: "absolute"}}>
             <canvas
                 ref={canvasRef}
+
                 onMouseDown={() => {
                     setDrawing(true);
                     step = [];
@@ -131,19 +155,10 @@ function MyCanvas({ postRef }) {
                 }}
                 onTouchMove={(e) => {
                     e.preventDefault();
-                    if (!isDrawing) return;
-                    const touch = e.touches[0];
-                    const rect = canvasRef.current.getBoundingClientRect();
-                    const offsetX = touch.clientX - rect.left;
-                    const offsetY = touch.clientY - rect.top;
-                    step.push({ offsetX, offsetY, color: contextRef.current.strokeStyle });
-                    contextRef.current.lineTo(offsetX, offsetY);
-                    contextRef.current.stroke();
-                    contextRef.current.beginPath();
-                    contextRef.current.moveTo(offsetX, offsetY);
+                    drawMobile()
                 }}
             />
-            <Palette contextRef={contextRef} undo={undo} redo={redo} clear={clear} />
+            <Palette contextRef={contextRef} undo={undo} redo={redo} clear={clear}/>
         </div>
     );
 }
