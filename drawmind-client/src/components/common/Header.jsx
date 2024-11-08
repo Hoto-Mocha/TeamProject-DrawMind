e79e98bf6f8f109828d3fd971371df41b121459f
Hoto-Mocha <mtj2010@naver.com>
2024-11-08 15:33:18 +0900

    헤더의 타이틀과 버튼들을 분리

--- drawmind-client/src/components/common/Header.jsx
+++ drawmind-client/src/components/common/Header.jsx
@@ -14,18 +14,20 @@ export default function Header() {
   return (
     <>
       <div className="headerBox">
-        <TfiMenuAlt className="menuIcon" onClick={handleShow}/>
+        <TfiMenuAlt className="menuIcon" onClick={handleShow} />
         <Offcanvas show={show} onHide={handleClose} style={{ "width": "70%" }}>
-          <Offcanvas.Header closeButton style={{ "borderBottom": "3px solid gray", "backgroundColor":"lightGray" }}>
+          <Offcanvas.Header closeButton style={{ "borderBottom": "3px solid gray", "backgroundColor": "lightGray" }}>
             <Offcanvas.Title>메뉴</Offcanvas.Title>
           </Offcanvas.Header>
-          <Offcanvas.Body style={{ "padding": "0", "display":"flex", "flexDirection":"column" }}>
+          <Offcanvas.Body style={{ "padding": "0", "display": "flex", "flexDirection": "column" }}>
             <Link className='offBoxNav' to='/login' onClick={handleClose}>로그인</Link>
             <Link className='offBoxNav' to='/register' onClick={handleClose}>회원 가입</Link>
           </Offcanvas.Body>
         </Offcanvas>
-        <Link className='headerTitle' to='/'><h2>MindDraw</h2></Link>
         <button className="btn btn-sm generalBtn">글쓰기</button>
+      </div>
+      <div className='titleContainer'>
+        <Link className='headerTitle' to='/'><h2>MindDraw</h2></Link>
       </div>
     </>
   )
