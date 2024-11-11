0d1248f4800204aa29951316c8e3199bf60da167
Hoto-Mocha <mtj2010@naver.com>
2024-11-11 14:29:37 +0900

    글쓰기 및 수정 화면 파일 경로 정리

--- drawmind-client/src/pages/write/Write.jsx
+++ drawmind-client/src/pages/write/Write.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import MyCanvas from "../MyCanvas.jsx";
-import "../css/Write.css"
+import "../../css/Write.css"
 
 function Write() {
     const postRef = useRef(null);  // 포스트를 참조하는 useRef
