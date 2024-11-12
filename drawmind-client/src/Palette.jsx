85fdd5d63b021445f8f4a421674f6436105cacdd
Hoto-Mocha <mtj2010@naver.com>
2024-11-12 11:13:43 +0900

    그림 에디터 디자인 수정

--- drawmind-client/src/Palette.jsx
+++ drawmind-client/src/Palette.jsx
@@ -1,27 +1,33 @@
 import Color from "./Color.jsx";
 import './css/Palette.css'
+import { FaUndoAlt, FaRedoAlt, FaTrashAlt } from "react-icons/fa";
+import { IoMdMove } from "react-icons/io";
+import { RiPencilFill } from "react-icons/ri";
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple', 'black'];
 
-function Palette({contextRef, undo, redo, clear, btnToggle, moveAvailable }) {
+function Palette({ contextRef, undo, redo, clear, btnToggle, moveAvailable }) {
     return (
-        <div className={'palette'}>
-            {/* 색깔 바꾸는 곳 */}
-            {
-                colors.map((color, index) => {
-                    return (
-                        <Color key={index}
-                               color={color}
-                               contextRef={contextRef}
-                        ></Color>
-                    )
-                })
-            }
-            <button onClick={undo}>undo</button>
-            <button onClick={redo}>redo</button>
-            <button onClick={clear}>clear</button>
-            {!moveAvailable && <button onClick={btnToggle}>move</button>}
-            {moveAvailable && <button onClick={btnToggle}>draw</button>}
+        <div className='palette'>
+            <div className="colorPalette">
+                {
+                    colors.map((color, index) => {
+                        return (
+                            <Color key={index}
+                                color={color}
+                                contextRef={contextRef}
+                            ></Color>
+                        )
+                    })
+                }
+            </div>
+            <div className="btnPalette">
+                <FaUndoAlt className="icon" onClick={undo} />
+                <FaRedoAlt className="icon" onClick={redo} />
+                <FaTrashAlt className="icon" onClick={clear} />
+                {!moveAvailable && <RiPencilFill className="icon" onClick={btnToggle} />}
+                {moveAvailable && <IoMdMove className="icon" onClick={btnToggle} />}
+            </div>
         </div>
     )
 }
