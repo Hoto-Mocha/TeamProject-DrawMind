4b5a437c1b969fde8b8d4692514bf7c800a658c5
Hoto-Mocha <mtj2010@naver.com>
2024-11-13 14:41:56 +0900

    더미 데이터 제거

--- drawmind-client/src/App.jsx
+++ drawmind-client/src/App.jsx
@@ -5,26 +5,6 @@ export default function App() {
 
   const data = [
     { "postSeq": 1, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:10" },
-    { "postSeq": 2, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:12" },
-    { "postSeq": 3, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 4, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 5, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 6, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 7, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 8, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 9, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 10, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 11, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 12, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 13, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 14, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 15, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 16, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 17, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 18, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 19, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 20, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
-    { "postSeq": 21, "postTitle": "게시글 제목", "writer": "관리자", "regDate": "2024-11-08 14:14" },
   ]
 
   const navigator = useNavigate()
