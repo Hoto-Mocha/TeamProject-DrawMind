00b50922a7bf36f32e8169925d09c150d60012ce
BarrelZebra2149 <1pq029@gmail.com>
2024-11-16 20:33:57 +0900

    새로고침 기능 버그 수정

--- drawmind-client/src/App.jsx
+++ drawmind-client/src/App.jsx
@@ -1,123 +1,118 @@
-import React, { useState, useEffect, useRef } from 'react';
-import toast, { toastConfig } from 'react-simple-toasts';
+import React, {useState, useEffect, useRef} from 'react';
+import toast, {toastConfig} from 'react-simple-toasts';
 import API from "./API";
 import "./css/App.css";
-import { Link } from 'react-router-dom';
+import rollingGif from "./images/Rolling.gif"
+import {Link} from 'react-router-dom';
 
 toastConfig({
-  theme: 'dark',
+    theme: 'dark',
 });
 
 export default function App() {
-  const nextPageRef = useRef(0);
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [isLastPage, setIsLastPage] = useState(false)
+    const nextPageRef = useRef(0);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [isLastPage, setIsLastPage] = useState(false);
+    const [isRefresh, setIsRefresh] = useState(false);
 
-  function isScrollEnd() {
-    return window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 10 //현재 화면의 맨 아래 가장자리가 페이지 맨 아래보다 0px 위 이내인 경우를 뜻함
-  }
+    useEffect(() => {
+        if (isRefresh) {
+            loadPage();
+        } else if (!loading && !isLastPage) {
+            loadPage();
+            const handleScroll = () => {
+                loadPage();
+            };
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+        }
+    }, [isRefresh, loading]);
 
-  function loadFirstPage() {
-    setPosts([])
-    setIsLastPage(false)
-    nextPageRef.current = 0
-    loadPage()
-  }
 
-  function loadPage() {
-    if (isScrollEnd()) {
-      setLoading(true);
+    function isScrollEnd() {
+        return window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 10 //현재 화면의 맨 아래 가장자리가 페이지 맨 아래보다 0px 위 이내인 경우를 뜻함
+    }
 
-      new Promise(resolve => {
-        resolve(
-          API.postList(nextPageRef.current)
-            .then((res) => {
-              const postList = res.data.body;
-              if (postList.length > 0) {
-                setPosts((prevPosts) => [...prevPosts, ...postList])
-                nextPageRef.current += 1
-              }
-              else {
-                setIsLastPage(true)
-                toast('마지막 페이지입니다.')
-              }
+    function loadFirstPage() {
+        setPosts([])
+        setIsRefresh(true);
+        setIsLastPage(false);
+        nextPageRef.current = 0
+        loadPage()
+    }
 
-              setTimeout(() => {
-                setLoading(false)
-              }, 10)
-            })
-            .catch((err) => {
-              console.log(err);
-              setIsLastPage(true)
-              toast('게시글 목록을 불러올 수 없습니다.');
+    function loadPage() {
+        if (isScrollEnd() || isRefresh) {
+            setLoading(true);
 
-              setTimeout(() => {
-                setLoading(false)
-              }, 10)
-            })
+            new Promise(resolve => {
+                resolve(
+                    API.postList(nextPageRef.current)
+                        .then((res) => {
+                            const postList = res.data.body;
+                            if (postList.length > 0) {
+                                setPosts((prevPosts) => [...prevPosts, ...postList])
+                                nextPageRef.current += 1
+                            } else {
+                                setIsLastPage(true)
+                                toast('마지막 페이지입니다.')
+                            }
+
+                            setTimeout(() => {
+                                setLoading(false)
+                            }, 500)
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            setIsLastPage(true)
+                            toast('게시글 목록을 불러올 수 없습니다.');
+
+                            setTimeout(() => {
+                                setLoading(false)
+                            }, 500)
+                        })
+                )
+            });
+            setIsRefresh(false);
+        }
+    }
+
+
+    function postItem(item) {
+        const writeDate = item.regDate.substring(0, 10)
+        const writeTime = item.regDate.substring(11, 16)
+        return (
+            <>
+                <div className="infoSection">
+                    <p>{item.postTitle}</p>
+                    <p>{item.writer}</p>
+                </div>
+                <p>{writeDate + ', ' + writeTime}</p>
+            </>
         )
-      });
     }
-  }
 
-  useEffect(() => {
-    if (!loading && !isLastPage) {
-      loadPage();
+    const linkStyle = {
+        textDecoration: "none",
+        color: "black"
     }
-  }, [loading]);
-  useEffect(() => {
-    if (!loading && !isLastPage) {
-      const handleScroll = () => {
-        loadPage();
-      };
 
-
-      window.addEventListener('scroll', handleScroll);
-      return () => window.removeEventListener('scroll', handleScroll);
-    }
-  }, [loading]);
-
-  function postItem(item) {
-    const writeDate = item.regDate.substring(0, 10)
-    const writeTime = item.regDate.substring(11, 16)
     return (
-      <>
-        <div className="infoSection">
-          <p>{item.postTitle}</p>
-          <p>{item.writer}</p>
-        </div>
-        <p>{writeDate + ', ' + writeTime}</p>
-      </>
+        <>
+            <div className='refreshBtnContainer'>
+                <button className='btn btn-primary btn-sm' onClick={loadFirstPage}>새로고침</button>
+            </div>
+            <div className="postList">
+                {posts.map((item, index) => {
+                    return (<Link to={`/contentView/${item.postSeq}`} className="postListItem" key={index}
+                                  style={linkStyle}>{postItem(item)}</Link>)
+                })}
+                {/* <button className="btn btn-primary" onClick={testBtnHandler}>API 테스트</button> */}
+            </div>
+            {!isLastPage && loading && <div className='app-loading'>
+                <img src={rollingGif} style={{width:'5vw', height:'5vw'}}/>
+            </div>}
+        </>
     )
-  }
-
-  const testBtnHandler = () => {
-    API.test()
-      .then((res) => {
-        console.log(res.data)
-      })
-  }
-
-  const linkStyle = {
-    textDecoration: "none",
-    color: "black"
-  }
-
-  return (
-    <>
-      <div className='refreshBtnContainer'>
-        <button className='btn btn-primary btn-sm' onClick={loadFirstPage}>새로고침</button>
-      </div>
-      <div className="postList">
-        {posts.map((item, index) => {
-          return (<Link to={`/contentView/${item.postSeq}`} className="postListItem" key={index} style={linkStyle}>{postItem(item)}</Link>)
-        })}
-        {/* <button className="btn btn-primary" onClick={testBtnHandler}>API 테스트</button> */}
-      </div>
-      {!isLastPage && loading && <div className='app-loading'>
-        <p>Loading...</p>
-      </div>}
-    </>
-  )
 }
